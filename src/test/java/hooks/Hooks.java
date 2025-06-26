0982122bcc366841803ec76f72185fa8bbfb2afa
Sumer <sumermadaan9@gmail.com>
2025-06-26 10:12:59 +0530

    Capture screenshot at test failure

--- src/test/java/hooks/Hooks.java
+++ src/test/java/hooks/Hooks.java
@@ -1,5 +1,12 @@
 package hooks;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
 import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
@@ -19,11 +26,13 @@ public class Hooks {
     }
    
    @AfterStep
-   public void checkStep(Scenario scenario)
+   public void checkStep(Scenario scenario) throws IOException
    {
 	   if(scenario.isFailed())
 	   {
-		   System.out.println("Code for screenshot");
+		   File srcFile = ((TakesScreenshot)DriverManager.getDriver()).getScreenshotAs(OutputType.FILE);
+		   
+		   FileUtils.copyFile(srcFile, new File("C:\\Users\\sumer\\eclipse-workspace\\JavaUIAutomation\\Screenshots\\screenshot.png"));
 	   }
    }
 
