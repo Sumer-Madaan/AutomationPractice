4af3dc6abe77a49738a54bbd4e647018dd3126a4
Sumer <sumermadaan9@gmail.com>
2025-06-27 10:31:27 +0530

    Utilizing properties files to invoke browser

--- src/test/java/utilities/DriverManager.java
+++ src/test/java/utilities/DriverManager.java
@@ -1,34 +1,59 @@
 package utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverManager {
-	
+
 	private static WebDriver driver;
-	
+
 	public static WebDriver getDriver()
 	{
-		if(driver == null)
-		{
-			System.setProperty("webdriver.driver.chrome", "C:\\Users\\sumer\\Softwares\\ChromeDriver\\chromedriver.exe");
-			driver = new ChromeDriver();
-			driver.manage().window().maximize();
-			driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
-		}
-		
-		return driver;
+		try {
+			if(driver == null)
+			{
+				FileInputStream fis = new FileInputStream("C:\\Users\\sumer\\eclipse-workspace\\JavaUIAutomation\\src\\test\\resources\\global.properties");
+				Properties prop = new Properties();
+				prop.load(fis);
+				String browser = prop.getProperty("browser");
+				System.out.println(browser);
+				if(browser.equalsIgnoreCase("chrome"))
+				{
+					System.setProperty("webdriver.chrome.driver", "C:\\Users\\sumer\\Softwares\\ChromeDriver\\chromedriver.exe");
+					driver = new ChromeDriver();
+
+				}
+				else if(browser.equalsIgnoreCase("edge"))
+				{
+					System.setProperty("webdriver.edge.driver", "C:\\Users\\sumer\\Softwares\\EdgeDriver\\edgedriver_win64\\msedgedriver.exe");
+					driver = new EdgeDriver();
+				}
+
+				driver.manage().window().maximize();
+				driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+			}
+	}catch (IOException e) {
+
+		e.printStackTrace();
 	}
-	
-	public static void quitDriver()
+
+	return driver;
+}
+
+public static void quitDriver()
+{
+	if(driver!=null)
 	{
-		if(driver!=null)
-		{
-			driver.quit();
-			driver = null;
-		}
+		driver.quit();
+		driver = null;
 	}
-	
-	
+}
+
+
 
 }
