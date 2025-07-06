d7f0e7963803b72a2b2b926b10d7d94201ec3c93
Sumer <sumermadaan9@gmail.com>
2025-07-06 15:14:54 +0530

    Added Headless browser execution capability

--- src/test/java/utilities/DriverManager.java
+++ src/test/java/utilities/DriverManager.java
@@ -6,7 +6,9 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 
 public class DriverManager {
 
@@ -24,13 +26,28 @@ public class DriverManager {
 				if(browser.equalsIgnoreCase("chrome"))
 				{
 					System.setProperty("webdriver.chrome.driver", "C:\\Users\\sumer\\Softwares\\ChromeDriver\\chromedriver.exe");
-					driver = new ChromeDriver();
+					ChromeOptions chromeOptions = new ChromeOptions();
+					
+					String headless = prop.getProperty("headless");
+					if(headless.equalsIgnoreCase("true"))
+					{
+						chromeOptions.addArguments("--headless=new");
+					}
+					
+					driver = new ChromeDriver(chromeOptions);
 
 				}
 				else if(browser.equalsIgnoreCase("edge"))
 				{
 					System.setProperty("webdriver.edge.driver", "C:\\Users\\sumer\\Softwares\\EdgeDriver\\edgedriver_win64\\msedgedriver.exe");
-					driver = new EdgeDriver();
+					EdgeOptions edgeOptions = new EdgeOptions();
+					String headless = prop.getProperty("headless");
+					if(headless.equalsIgnoreCase("true"))
+					{
+						edgeOptions.addArguments("--headless=new");
+					}
+					
+					driver = new EdgeDriver(edgeOptions);
 				}
 
 				driver.manage().window().maximize();
