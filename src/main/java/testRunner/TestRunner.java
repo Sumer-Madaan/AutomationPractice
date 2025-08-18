68b570079418b7f7c18c6191800a31fa5ed1a19f
Sumer <sumermadaan9@gmail.com>
2025-08-18 20:44:19 +0530

    Test Enhancement

--- src/main/java/testRunner/TestRunner.java
+++ src/main/java/testRunner/TestRunner.java
@@ -11,7 +11,7 @@ plugin = {"pretty","html:target/report.html","com.aventstack.extentreports.cucum
 public class TestRunner extends AbstractTestNGCucumberTests{
 	
 	@Override
-	@DataProvider(parallel=true)
+	@DataProvider(parallel=false)
 	public Object[][] scenarios()
 	{
 		return super.scenarios();
